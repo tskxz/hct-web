883c2dda8d8f1b2a4ded78bfb117a756d1e483e7
Tanjil <tanjilkh@gmail.com>
2026-05-28 23:30:29 +0100

    web: tema light

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -59,7 +59,10 @@ export default function RootLayout({
         <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       </head>
       <body
-        className={cn(inter.className, "dark:bg-slate-900 dark:text-slate-400")}
+        className={cn(
+          inter.className,
+          "bg-white text-slate-900 dark:bg-slate-900 dark:text-slate-400",
+        )}
       >
         <ThemeSwitcher />
         <Navbar />
