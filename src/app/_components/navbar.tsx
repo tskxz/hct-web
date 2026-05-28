94bcf5886681c90a380d90c318eb5b2c4dcd1810
Tanjil <tanjilkh@gmail.com>
2026-05-28 13:23:12 +0100

    web: navbar apresentacao

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -68,6 +68,47 @@ export function Navbar() {
               </div>
             )}
           </div>
+          <div className="relative inline-block text-left">
+            <button
+              onClick={() => setIsOpen(!isOpen)}
+              type="button"
+              className="inline-flex items-center gap-x-1.5 rounded-md bg-white dark:bg-slate-800 px-3 py-2 text-sm font-semibold text-gray-900 dark:text-white shadow-sm ring-1 ring-inset ring-gray-300 dark:ring-slate-700 hover:bg-gray-50 dark:hover:bg-slate-700 transition-colors"
+            >
+              Apresentação
+              <svg className="-mr-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+                <path fillRule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clipRule="evenodd" />
+              </svg>
+            </button>
+
+
+            {isOpen && (
+              <div className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white dark:bg-slate-800 shadow-lg ring-1 ring-black ring-opacity-5 dark:ring-slate-700 focus:outline-none">
+                <div className="py-1">
+                  <Link
+                    href="/apresentacao_pessoal"
+                    className="block px-4 py-2 text-sm text-gray-700 dark:text-slate-200 hover:bg-gray-100 dark:hover:bg-slate-700"
+                    onClick={() => setIsOpen(false)}
+                  >
+                    Pessoal
+                  </Link>
+                  <Link
+                    href="/apresentacao_academica_profissional"
+                    className="block px-4 py-2 text-sm text-gray-700 dark:text-slate-200 hover:bg-gray-100 dark:hover:bg-slate-700"
+                    onClick={() => setIsOpen(false)}
+                  >
+                    Académica e profissional
+                  </Link>
+                  <Link
+                    href="/apresentacao_skills"
+                    className="block px-4 py-2 text-sm text-gray-700 dark:text-slate-200 hover:bg-gray-100 dark:hover:bg-slate-700"
+                    onClick={() => setIsOpen(false)}
+                  >
+                    Skills
+                  </Link>
+                </div>
+              </div>
+            )}
+          </div>
         </div>
 
       </div>
